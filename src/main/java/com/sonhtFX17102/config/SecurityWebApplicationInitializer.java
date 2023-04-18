cadf9b3f99573b6e30aec032242799aafae97fab
son_kute <thanhson@example.com>
2023-04-18 16:15:49 +0700

    config security using xml

--- src/main/java/com/sonhtFX17102/config/SecurityWebApplicationInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.sonhtFX17102.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer 
-						extends AbstractSecurityWebApplicationInitializer {
-
-}
